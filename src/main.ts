a1643c5e1b8f3b1f5ff8c1211fd7db2c15de0340
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2025-06-26 11:33:24 -0300

    feat: add config serverless

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
+// vercel.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Context, Handler } from 'aws-lambda';
+import { createServer, proxy } from 'aws-serverless-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-  await app.listen(process.env.PORT ?? 3000);
+let cachedServer: any = null;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const adapter = new ExpressAdapter(expressApp);
+    const app = await NestFactory.create(AppModule, adapter);
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+      origin: '*',
+      methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+    await app.init();
+    cachedServer = createServer(expressApp);
+  }
+  return cachedServer;
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context) => {
+  const server = await bootstrapServer();
+  return proxy(server, event, context, 'PROMISE').promise;
+};
