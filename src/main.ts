89fb08c080f8bedfeaaa253ed07ae17c12e6a618
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2025-06-26 11:41:59 -0300

    feat: agora vai

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,18 @@
-// vercel.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { Context, Handler } from 'aws-lambda';
-import { createServer, proxy } from 'aws-serverless-express';
-import * as express from 'express';
 import { AppModule } from './app.module';
 
-let cachedServer: any = null;
-
-async function bootstrapServer() {
-  if (!cachedServer) {
-    const expressApp = express();
-    const adapter = new ExpressAdapter(expressApp);
-    const app = await NestFactory.create(AppModule, adapter);
-    app.useGlobalPipes(new ValidationPipe());
-    app.enableCors({
-      origin: '*',
-      methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    });
-    await app.init();
-    cachedServer = createServer(expressApp);
-  }
-  return cachedServer;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-export const handler: Handler = async (event: any, context: Context) => {
-  const server = await bootstrapServer();
-  return proxy(server, event, context, 'PROMISE').promise;
-};
+bootstrap();
